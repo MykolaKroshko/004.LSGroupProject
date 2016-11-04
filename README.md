1389a9d857a51ef6b39bf6ca8c0cb14e6ec232fa
Elena Malyugina <malyugina.el@yandex.ru>
2016-11-04 20:43:45 +0300

    закрытие окна по кресту

---
+++ README.md
@@ -0,0 +1,11 @@
+LoftSchool web advansed 09.2016
+
+Project #2 (Group web-development)
+
+Getting started:
+
+1. clone this repo
+2. cd path/to/
+3. npm install git+https://git@github.com/gulpjs/gulp.git#4.0 gulpjs/gulp-cli -g  // Install the latest Gulp + Gulp CLI tools globally
+4. npm install
+6. run "gulp" command to start
---
+++ source/js/main.js
@@ -0,0 +1,52 @@
+
+
+var eventListeners = (function () {
+    var init = function () {
+        _setUpListeners();
+    };
+
+    var _setUpListeners = function () {
+
+
+        $('#edit').on('click', _editHeader);
+        $('#addAlbum').on('click', _addAlbum);
+        $('#addPhoto').on('click',  _addPhoto);
+
+        $('.popUp__close').on('click', function(){
+            _closePopUp();
+        });
+
+    };
+
+    var _editHeader = function (ev) {
+        ev.preventDefault();
+        $('#popUp_editHeader').addClass("popUp__overlay-show");
+
+    };
+
+    var _addAlbum = function (ev) {
+        ev.preventDefault();
+        $('#popUp_addAlbum').addClass("popUp__overlay-show");
+
+    };
+
+    var _addPhoto = function (ev) {
+        ev.preventDefault();
+        $('#popUp_addPhoto').addClass("popUp__overlay-show");
+
+    };
+
+    var _closePopUp = function () {
+        //console.log('ddd');
+        $('.popUp__overlay').removeClass("popUp__overlay-show");
+    };
+
+
+    return {
+        init: init
+    };
+
+})();
+
+eventListeners.init();
+
