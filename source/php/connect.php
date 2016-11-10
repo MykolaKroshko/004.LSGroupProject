d72dc98f7cf979368ee059a49548255938f3cf38
xamjo <xamjofoux@gmail.com>
2016-11-10 12:32:26 +0200

    added load more

--- source/php/connect.php
+++ source/php/connect.php
@@ -1,7 +1,7 @@
 <?
 $host="localhost";
 $dbname="ct61014_photo";
-$user="ct61014_photo";
-$pass="cGs7k7lW";
+$user="root";
+$pass="";
 $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 header('Access-Control-Allow-Origin: *');
