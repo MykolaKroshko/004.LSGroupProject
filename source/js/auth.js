86724cefe5a284cd87e9dba6467925a36fcb4fb7
xamjo <xamjofoux@gmail.com>
2016-11-11 17:02:58 +0200

    added redistration

--- source/js/auth.js
+++ source/js/auth.js
@@ -1,16 +1,20 @@
 $("#authorize").on('click',function(){
   var $this = $(this),
-    $login = $this.closest("form").find("[name=login]").val().trim(),
-    $pas = $this.closest("form").find("[name=password]").val().trim();
-    if($login.length && $pas.length){
+    $login = $this.closest("form").find("[name=login]"),
+    $pas = $this.closest("form").find("[name=password]"),
+    login = $login.val().trim(),
+    pas = $pas.val().trim();
+    if(login.length && pas.length){
       $.ajax({
         type: "POST",
         url: "./assets/php/auth.php",
-        data: "login="+$login+"&password="+$pas,
+        data: "login="+login+"&password="+pas,
         success: function(msg){
           var res = JSON.parse(msg);
           if (!res){
             alert("Wrong login and/or password");
+            $login.val("");
+            $pas.val("");
           } else {
             localStorage.LSGroupProject_userID = res.id_user;
             location.href="/main.html";
